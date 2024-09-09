4f8d8ab120df4d742217d285388f1a02302b5797
Shahriar <bidyuthasan9@gmail.com>
2024-09-09 07:27:15 +0600

    adding form validation

--- Frontend/src/screens/AdressForm.jsx
+++ Frontend/src/screens/AdressForm.jsx
@@ -19,24 +19,24 @@ export default function AdressForm() {
         <div className='formBody'>
             <Navbar />
             <div className='row'>
-                <form className='col-8 offset-3' onSubmit={handleSubmission}>
+                <form className='col-8 offset-3 needs-validation' novalidate onSubmit={handleSubmission}>
                     <h2 className='offset-2'>
                         Delievery Details
                     </h2>
                     <div className="form-row m-2">
                         <div className="form-group col-sm-12 col-md-8 col-lg-8">
                             <label htmlFor="inputName">Receiver Name</label>
-                            <input type="text" className="form-control" id="inputname" placeholder="Name" />
+                            <input type="text" className="form-control" id="inputname" placeholder="Name" required />
                         </div>
                     </div>
                     <div className="form-group col-sm-12 col-md-6 col-lg-8 m-2">
                         <label htmlFor="inputAddress">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="116-North, CUET" />
+                        <input type="text" className="form-control" id="inputAddress" placeholder="116-North, CUET" required />
                     </div>
                     <div className="form-row m-2">
                         <div className="form-group col-md-6">
                             <label htmlFor="Mobile">Mobile</label>
-                            <input type="text" className="form-control" id="inputmobile" placeholder='+8801*********' />
+                            <input type="text" className="form-control" id="inputmobile" placeholder='+8801*********' required />
                         </div>
                         <div className="form-group col-md-6">
                             <label htmlFor="Mobile">Alternative Contact</label>
@@ -46,7 +46,7 @@ export default function AdressForm() {
                     <div className="form-group m-2">
                         <div className="form-check">
                             <input className="form-check-input" type="checkbox" id="gridCheck" />
-                            <label className="form-check-label" htmlFor="gridCheck">
+                            <label className="form-check-label" htmlFor="gridCheck" >
                                 Will Cutlery be provided if available?
                             </label>
                         </div>
